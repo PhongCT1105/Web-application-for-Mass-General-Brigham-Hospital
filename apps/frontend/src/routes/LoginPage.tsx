1631b305f054af052c00bdca0de907142cb1b062
Henry Solomon <solomonhenry37@gmail.com>
2024-04-02 20:52:46 -0400

    fixed hero section overlapping with header

--- apps/frontend/src/routes/LoginPage.tsx
+++ apps/frontend/src/routes/LoginPage.tsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
   return (
     <>
       <Header />
-      <div className="container relative hidden h-[700px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative hidden h-[700px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 w-full gap-5">
         <div className="relative hidden h-full flex-col text-white lg:flex dark:border-r">
           <div className="absolute inset-0" />
           <div className="relative z-20 flex items-center text-2xl font-medium">
