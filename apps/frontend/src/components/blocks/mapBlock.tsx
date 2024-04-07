2168d15322f45caf28af327e44cfde97c272ae16
Alexander Lap <78438296+alexanderjlap@users.noreply.github.com>
2024-04-06 20:43:52 -0400

    made 5 map displays

--- apps/frontend/src/components/blocks/mapBlock.tsx
+++ apps/frontend/src/components/blocks/mapBlock.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import L, { CRS, LatLngBoundsExpression, Map, Polyline, Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import lowerLevel1 from "@/assets/00_thelowerlevel1.png";
-import lowerLevel2 from "@/assets/00_thelowerlevel2.png";
+import lowerLevelMap1 from "@/assets/00_thelowerlevel1.png";
+import lowerLevelMap2 from "@/assets/00_thelowerlevel2.png";
 import theFirstFloor from "@/assets/01_thefirstfloor.png";
 import theSecondFloor from "@/assets/02_thesecondfloor.png";
 import theThirdFloor from "@/assets/03_thethirdfloor.png";
@@ -13,143 +13,136 @@ import axios from "axios";
 import { Graph } from "@/util/graph.tsx";
 import { Node } from "../../util/node.tsx";
 import { BFS } from "@/util/bfs.tsx";
+//import { Edge } from "../../util/edge.tsx";
+// import {mapReq} from "common/src/mapReq.ts";
 
 interface HospitalData {
   nodeID: string;
   name: string;
   geocode: string;
-  floor: string;
-}
-
-interface FloorMaps {
-  [key: string]: string;
 }
 
 let hospitalGraph = new Graph();
 
 const hospitalData: HospitalData[] = [];
 
+// Define the map component
 export const MapBlock: React.FC = () => {
   const mapRef = useRef<Map | null>(null);
+  // const [path, setPath] = useState<Polyline | null>(null);
   const [paths, setPaths] = useState<Polyline[]>([]);
   const [hospitalDataString, setHospitalDataString] = useState<string[]>([]);
-  const [currentFloor, setCurrentFloor] = useState<string>("");
   const [graph, setGraph] = useState<Graph>(new Graph());
+  const [currentFloor, setCurrentFloor] = useState("lowerLevel1");
 
-  const floorMaps: FloorMaps = {
-    lowerLevel1: lowerLevel1,
-    lowerLevel2: lowerLevel2,
+  const floorMaps: { [key: string]: string } = {
+    lowerLevel1: lowerLevelMap1,
+    lowerLevel2: lowerLevelMap2,
     theFirstFloor: theFirstFloor,
     theSecondFloor: theSecondFloor,
     theThirdFloor: theThirdFloor,
   };
 
   const drawNodes = async () => {
-    try {
-      const { data: edgeData } = await axios.get("/api/mapreq/edges");
-      const { data: nodeData } = await axios.get("/api/mapreq/nodes");
+    const { data: edgeData } = await axios.get("/api/mapreq/edges");
+    const { data: nodeData } = await axios.get("/api/mapreq/nodes");
 
-      console.log("Edge Data:", edgeData);
+    console.log(edgeData);
 
-      const stringData: string[] = [];
+    const stringData: string[] = [];
 
-      const newGraph: Graph = new Graph();
-      for (let i = 0; i < nodeData.length; i++) {
-        hospitalData.push({
-          nodeID: nodeData[i].nodeID,
-          name: nodeData[i].longName,
-          geocode: `${nodeData[i].xcoord},${nodeData[i].ycoord}`,
-          floor: nodeData[i].floor,
-        });
-
-        console.log("Hospital Data:", hospitalData);
-        stringData.push(nodeData[i].longName);
-
-        newGraph.addNode(
-          new Node(
-            nodeData[i].nodeID,
-            parseInt(nodeData[i].xcoord),
-            parseInt(nodeData[i].ycoord),
-            nodeData[i].floor,
-            nodeData[i].building,
-            nodeData[i].nodeType,
-            nodeData[i].longName,
-            nodeData[i].shortName,
-            new Set<Node>(),
-          ),
-        );
-      }
-
-      console.log("Nodes added");
-      console.log("New Graph:", newGraph);
-
-      for (let i = 0; i < edgeData.length; i++) {
-        console.log(
-          "Adding neighbors:",
-          edgeData[i].startNodeID,
-          edgeData[i].endNodeID,
-        );
-        newGraph.addNeighbors(edgeData[i].startNodeID, edgeData[i].endNodeID);
-      }
-
-      console.log("Edges added");
-      console.log("Updated Graph:", newGraph);
-      setHospitalDataString(stringData);
-      setGraph(newGraph);
-
-      hospitalGraph = newGraph;
-
-      console.log("Hospital Data:", hospitalData);
-      console.log("Hospital Graph:", hospitalGraph);
-
-      // Initialize the map with the correct initial floor image
-      const initialFloorImage = floorMaps["lowerLevel1"]; // Change this to the desired initial floor
-      const map: Map = L.map("map-container", {
-        crs: CRS.Simple,
-        minZoom: -2,
-        maxZoom: 2,
-        zoomControl: true,
-      }).setView([3400, 5000], -2);
-
-      mapRef.current = map;
-
-      const bounds: LatLngBoundsExpression = [
-        [0, 0],
-        [3400, 5000], // change to resolution of the image
-      ];
-
-      L.imageOverlay(initialFloorImage, map.getBounds()).addTo(map);
-      map.setMaxBounds(bounds);
-
-      hospitalData.forEach((hospital) => {
-        const customIcon = new Icon({
-          iconUrl: RedDot,
-          iconSize: [12, 12],
-          iconAnchor: [6, 6],
-        });
-        const [lat, lng] = hospital.geocode.split(",").map(parseFloat);
-        const nLat = 3400 - lng;
-        const marker = L.marker([nLat, lat], { icon: customIcon }).addTo(map);
-
-        const popupContent = `<b>${hospital.name}</b><br/>Latitude: ${lat}, Longitude: ${lng}`;
-        marker.bindPopup(popupContent);
-
-        marker.on("click", function (this: L.Marker) {
-          if (!this.isPopupOpen()) {
-            this.openPopup();
-          }
-        });
+    const newGraph: Graph = new Graph();
+    for (let i = 0; i < nodeData.length; i++) {
+      hospitalData.push({
+        nodeID: nodeData[i].nodeID,
+        name: nodeData[i].longName,
+        geocode: `${nodeData[i].xcoord},${nodeData[i].ycoord}`,
       });
 
-      setCurrentFloor("lowerLevel1");
-    } catch (error) {
-      console.error("Error while drawing nodes:", error);
+      console.log(hospitalData);
+      stringData.push(nodeData[i].longName);
+
+      newGraph.addNode(
+        new Node(
+          nodeData[i].nodeID,
+          parseInt(nodeData[i].xcoord),
+          parseInt(nodeData[i].ycoord),
+          nodeData[i].floor,
+          nodeData[i].building,
+          nodeData[i].nodeType,
+          nodeData[i].longName,
+          nodeData[i].shortName,
+          new Set<Node>(),
+        ),
+      );
     }
+
+    console.log("Nodes added");
+    console.log(newGraph);
+
+    for (let i = 0; i < edgeData.length; i++) {
+      console.log(edgeData[i].startNodeID + edgeData[i].endNodeID);
+      newGraph.addNeighbors(edgeData[i].startNodeID, edgeData[i].endNodeID);
+    }
+
+    console.log("Edges added??????");
+    console.log(newGraph);
+    setHospitalDataString(stringData);
+    setGraph(newGraph);
+
+    hospitalGraph = newGraph;
+
+    console.log(hospitalData);
+    console.log(hospitalGraph);
+
+    const map: Map = L.map("map-container", {
+      crs: CRS.Simple,
+      minZoom: -2,
+      maxZoom: 2,
+      zoomControl: true,
+    }).setView([3400, 5000], -2);
+
+    mapRef.current = map;
+
+    const bounds: LatLngBoundsExpression = [
+      [0, 0],
+      [3400, 5000], // change to resolution of the image
+    ];
+
+    L.imageOverlay(lowerLevelMap1, bounds).addTo(map);
+
+    map.setMaxBounds(bounds);
+
+    hospitalData.forEach((hospital) => {
+      const customIcon = new Icon({
+        iconUrl: RedDot,
+        iconSize: [12, 12],
+        iconAnchor: [6, 6],
+      });
+      const [lat, lng] = hospital.geocode.split(",").map(parseFloat);
+      const nLat = 3400 - lng;
+      const marker = L.marker([nLat, lat], { icon: customIcon }).addTo(map);
+
+      // Add a click event handler to toggle popup visibility
+      const popupContent = `<b>${hospital.name}</b><br/>Latitude: ${lat}, Longitude: ${lng}`;
+      marker.bindPopup(popupContent);
+
+      marker.on("click", function (this: L.Marker) {
+        // Specify the type of 'this' as L.Marker
+        if (!this.isPopupOpen()) {
+          // Check if the popup is not already open
+          this.openPopup(); // Open the popup when the marker is clicked
+        }
+      });
+
+      return () => {
+        map.remove();
+      };
+    });
   };
 
   useEffect(() => {
     drawNodes();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function addToPaths(newPath: Polyline) {
@@ -186,17 +179,12 @@ export const MapBlock: React.FC = () => {
 
     const nodes: Node[] = BFS.run(graph, startNode, endNode);
 
-    console.log("Nodes in full path:", nodes);
+    console.log(nodes);
     for (let i = 0; i < nodes.length - 1; i++) {
-      console.log(
-        "Drawing path from",
-        nodes[i].nodeID,
-        "to",
-        nodes[i + 1].nodeID,
-      );
+      console.log("A path should be created now");
       drawPath(nodes[i].nodeID, nodes[i + 1].nodeID);
     }
-    console.log("Full path drawn");
+    console.log("done :D");
   }
 
   function drawLine(
@@ -209,7 +197,7 @@ export const MapBlock: React.FC = () => {
     const newPath = L.polyline([startCoordinates, endCoordinates], {
       color: "blue",
     }).addTo(map);
-    addToPaths(newPath);
+    addToPaths(newPath); // Add the new path to the paths list
   }
 
   function clearLines() {
@@ -221,7 +209,8 @@ export const MapBlock: React.FC = () => {
   }
 
   async function handleSearch(start: string, end: string) {
-    console.log("Search started from", start, "to", end);
+    console.log(start);
+    console.log(end);
     drawFullPath(graph, start, end);
   }
 
@@ -238,7 +227,13 @@ export const MapBlock: React.FC = () => {
     });
 
     if (floorMaps[floorName]) {
-      L.imageOverlay(floorMaps[floorName], map.getBounds()).addTo(map);
+      const initialFloorImage = floorMaps[floorName];
+      const bounds: LatLngBoundsExpression = [
+        [0, 0],
+        [3400, 5000], // Update with actual resolution
+      ];
+      L.imageOverlay(initialFloorImage, bounds).addTo(map);
+      map.setMaxBounds(bounds);
     }
   }
 
@@ -313,5 +308,3 @@ export const MapBlock: React.FC = () => {
     </div>
   );
 };
-
-console.log("MapBlock component loaded");
