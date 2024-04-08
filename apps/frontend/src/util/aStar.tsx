068d32ea59e7b025c7188b9d8dc9f19dbdc62770
tdyang <tracy3846@gmail.com>
2024-04-08 00:56:30 -0400

    more astar fiddling, still need to send path, also still having issues with versions and dependencies wahhhhhh

--- apps/frontend/src/util/aStar.tsx
+++ apps/frontend/src/util/aStar.tsx
@@ -65,3 +65,84 @@ export class aStar {
     return [];
   }
 }
+//
+// import { Graph } from "./Graph.tsx";
+// import { Node } from "@/util/Node.tsx";
+// //import { Node } from "./Node.tsx";
+// //import { Queue } from "queue-typescript";
+//
+// export class aStar {
+//     // run will perform all the logic of the A* implementation
+//     static run(graph: Graph, startNodeID: string, endNodeID: string) {
+//         console.log(graph);
+//         console.log(startNodeID);
+//         console.log(endNodeID);
+//
+//         // translating nodes
+//         const startNode = graph.nodes.get(startNodeID)!;
+//         const endNode = graph.nodes.get(endNodeID)!;
+//
+//         // setting up open, cameFrom, costs
+//         const open: Node[] = [startNode];
+//         const cameFrom = new Map<string, string>();
+//         const gCost = new Map<Node, number>();
+//         const fCost = new Map<Node, number>();
+//
+//         // initializing startNodeID, path is not found
+//         graph.nodes.forEach((node) => {
+//             gCost.set(node, Infinity);
+//             fCost.set(node, Infinity);
+//         });
+//         let aStarFound = false;
+//
+//         gCost.set(startNode, 0); // dist from origin
+//         fCost.set(startNode, this.calcDist(startNode, endNode)); // dist to goal
+//
+//         while (open.length != 0) {
+//             const curr = open.reduce((minNode, node) =>
+//                 fCost.get(node)! < fCost.get(minNode)! ? node : minNode,
+//             ); // thanks chatgpt
+//
+//             if (curr === endNode) {
+//                 aStarFound = true;
+//             }
+//
+//             open.splice(open.indexOf(curr), 1);
+//
+//             for (const neighNode of curr.neighbors) {
+//                 const newCost = gCost.get(curr)! + this.calcDist(curr, neighNode);
+//
+//                 if (newCost < gCost.get(neighNode)! ?? Infinity) {
+//                     gCost.set(neighNode, newCost);
+//                     fCost.set(neighNode, newCost + this.calcDist(neighNode, endNode));
+//                     cameFrom.set(neighNode.nodeID, curr.nodeID);
+//                 }
+//             }
+//         }
+//
+//         if (!aStarFound) {
+//             console.log("No path found");
+//             return []; // Return an empty array to indicate that the path does not exist
+//         }
+//
+//         //return this.reconstructPath(); // fix later
+//     }
+//
+//     static calcDist(currNode: Node, goalNode: Node): number {
+//         return Math.hypot(
+//             goalNode.xcoord - currNode.xcoord,
+//             goalNode.ycoord - currNode.ycoord,
+//         );
+//     }
+//
+//     // reconstructPath will take in relevant parameters and return an array of Nodes
+//     // static reconstructPath(
+//     //     cameFrom: Map<string, string>,
+//     //     startNodeID: string,
+//     //     endNodeID: string,
+//     //     graph: Graph,
+//     // ): Node[] {
+//     //     const path: Node[] = []; // returning an array of Nodes
+//     //     return path;
+//     // }
+// }
