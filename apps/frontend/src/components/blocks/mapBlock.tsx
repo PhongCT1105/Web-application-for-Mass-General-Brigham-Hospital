6bc5f3f0c4e17f9ed49b89466d0ec1bd15fb62f2
Alexander Lap <78438296+alexanderjlap@users.noreply.github.com>
2024-04-07 22:15:42 -0400

    still having trouble with nodes

--- apps/frontend/src/components/blocks/mapBlock.tsx
+++ apps/frontend/src/components/blocks/mapBlock.tsx
@@ -103,7 +103,6 @@ export const MapBlock: React.FC = () => {
     }
     setHospitalDataString(stringData);
     setGraph(newGraph);
-
     hospitalGraph = newGraph;
 
     console.log(hospitalData);
