9a88df5d2871f2009f3872740bb123794b0f6003
Alexander Kraemling <alexander.kraemling@gmail.com>
2024-04-11 05:03:53 -0400

    Cleaned AboutUsPage.tsx

--- apps/frontend/src/routes/AboutUsPage.tsx
+++ apps/frontend/src/routes/AboutUsPage.tsx
@@ -8,24 +8,6 @@ export default function AboutUsPage() {
   return (
     <>
       <Header highlighted={"/about-us"} />
-
-      {/*<div>*/}
-      {/*  <h1 className="mt-aboutUsOffset text-3xl mb-10">*/}
-      {/*    Don't mind me. I'm just tending to my garden.*/}
-      {/*  </h1>*/}
-      {/*  <div className="">*/}
-      {/*    <img*/}
-      {/*      src={wongAsters}*/}
-      {/*      alt="Wong Asters"*/}
-      {/*      className="inline-block  max-w-[811px] mb-4"*/}
-      {/*    />*/}
-      {/*    <img*/}
-      {/*      src={wongSunflower}*/}
-      {/*      alt="Wong Sunflower"*/}
-      {/*      className="inline-block max-w-[900px] mb-4"*/}
-      {/*    />*/}
-      {/*  </div>*/}
-      {/*</div>*/}
     </>
   );
 }
