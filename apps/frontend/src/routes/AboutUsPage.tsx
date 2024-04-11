a3b1e400c3d98c32f7e66fd1177055756e0d63bf
Alexander Kraemling <alexander.kraemling@gmail.com>
2024-04-11 02:46:47 -0400

    Merge check

--- apps/frontend/src/routes/AboutUsPage.tsx
+++ apps/frontend/src/routes/AboutUsPage.tsx
@@ -1,31 +1,31 @@
 import "../styles/example.route.css";
 import "../styles/globals.css";
 import { Header } from "@/components/blocks/header.tsx";
-import wongAsters from "@/assets/wongSunFlower.png";
-import wongSunflower from "@/assets/wongAsters.png";
+// import wongAsters from "@/assets/wongSunFlower.png";
+// import wongSunflower from "@/assets/wongAsters.png";
 
 export default function AboutUsPage() {
   return (
     <>
       <Header highlighted={"/about-us"} />
 
-      <div>
-        <h1 className="mt-aboutUsOffset text-3xl mb-10">
-          Don't mind me. I'm just tending to my garden.
-        </h1>
-        <div className="">
-          <img
-            src={wongAsters}
-            alt="Wong Asters"
-            className="inline-block  max-w-[811px] mb-4"
-          />
-          <img
-            src={wongSunflower}
-            alt="Wong Sunflower"
-            className="inline-block max-w-[900px] mb-4"
-          />
-        </div>
-      </div>
+      {/*<div>*/}
+      {/*  <h1 className="mt-aboutUsOffset text-3xl mb-10">*/}
+      {/*    Don't mind me. I'm just tending to my garden.*/}
+      {/*  </h1>*/}
+      {/*  <div className="">*/}
+      {/*    <img*/}
+      {/*      src={wongAsters}*/}
+      {/*      alt="Wong Asters"*/}
+      {/*      className="inline-block  max-w-[811px] mb-4"*/}
+      {/*    />*/}
+      {/*    <img*/}
+      {/*      src={wongSunflower}*/}
+      {/*      alt="Wong Sunflower"*/}
+      {/*      className="inline-block max-w-[900px] mb-4"*/}
+      {/*    />*/}
+      {/*  </div>*/}
+      {/*</div>*/}
     </>
   );
 }
