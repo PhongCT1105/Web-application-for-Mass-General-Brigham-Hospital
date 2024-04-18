696bf78c673c9cae8d341058844239aae8fae105
minaboktor2628 <148801385+minaboktor2628@users.noreply.github.com>
2024-04-18 00:52:46 -0400

    made paths appear without clicking buttons

--- apps/frontend/src/routes/MapEditingPage.tsx
+++ apps/frontend/src/routes/MapEditingPage.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "leaflet/dist/leaflet.css";
+import { MapEditorRefactor } from "@/routes/map-editor/mapEditorBlockRefactor.tsx";
 // import { Header } from "@/components/blocks/header.tsx";
-import { MapEditor } from "@/routes/map-editor/mapEditorBlock.tsx";
+// import { MapEditor } from "@/routes/map-editor/mapEditorBlock.tsx";
 
 // Define the map component
 const MapEditingPage = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
       {/*<Header highlighted={"/map-editor"} /> /!* Add the Header component *!/*/}
-      <MapEditor />
+      <MapEditorRefactor />
     </div>
   );
 };
