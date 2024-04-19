973adf9fa681fec9e8311d12dee8f75d9d842aa8
Phong Cao <104048448+PhongCT1105@users.noreply.github.com>
2024-04-19 14:40:44 -0400

    Update Bar chart

---
+++ apps/frontend/src/components/Graph/LineGraph.tsx
@@ -0,0 +1,32 @@
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { lineChartData } from "@/data/lineChartData";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+function LineGraph() {
+  const options = {};
+  return (
+    <>
+      <Line options={options} data={lineChartData} />
+    </>
+  );
+}
+
+export default LineGraph;
