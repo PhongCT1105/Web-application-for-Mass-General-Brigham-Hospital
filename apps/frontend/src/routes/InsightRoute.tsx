619d0c90bbe474d1f3d93fea6d5e8207e70006b6
Phong Cao <104048448+PhongCT1105@users.noreply.github.com>
2024-04-19 14:52:02 -0400

    Update Insight Route

---
+++ apps/frontend/src/routes/InsightRoute.tsx
@@ -0,0 +1,21 @@
+import "../styles/example.route.css";
+import "../styles/globals.css";
+import LineGraph from "@/components/Graph/LineGraph.tsx";
+import BarGraph from "@/components/Graph/BarGraph.tsx";
+
+function InsightRoute() {
+  return (
+    <>
+      <div>
+        <h1>Line Graph of website usage in last year</h1>
+        <LineGraph />
+      </div>
+      <div>
+        <h1>Bar Chart of usage of every Service Requests</h1>
+        <BarGraph />
+      </div>
+    </>
+  );
+}
+
+export default InsightRoute;
