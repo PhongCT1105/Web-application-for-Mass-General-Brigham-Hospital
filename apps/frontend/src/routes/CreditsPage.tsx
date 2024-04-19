83326293704cb3f414e69d4f5965cdbb866e2fe7
echoct21 <41646433+echoct21@users.noreply.github.com>
2024-04-19 18:35:24 -0400

    Added blank credits page, and used tabs to implement it

---
+++ apps/frontend/src/routes/CreditsPage.tsx
@@ -0,0 +1,37 @@
+import "../styles/example.route.css";
+import "../styles/globals.css";
+import WPI_Logo from "@/assets/WPI_Logo.png";
+import { Separator } from "@/components/ui/separator.tsx";
+
+// import wongAsters from "@/assets/wongSunFlower.png";
+// import wongSunflower from "@/assets/wongAsters.png";
+
+const CreditsPage = () => {
+  return (
+    <div>
+      <div className="about-the-team w-5/6 pt-10 pb-5 gap-5 m-auto">
+        <h4 className={"text-center text-md text-muted-foreground"}>
+          This application was developed by a team of students at Worcester
+          Polytechnic Institute as part of the CS3733-D24 Software Engineering
+          course. The team was led by Professor Wilson Wong and Team Coach Ari
+          Schechter and Katy Stuparu.
+        </h4>
+        <Separator></Separator>
+
+        <h6 style={{ textAlign: "center", marginTop: "75px" }}>
+          WPI Computer Science Department, CS3733-D24 Software Engineering
+        </h6>
+        <h6 style={{ textAlign: "center", marginTop: "20px" }}>
+          ©The Brigham & Women’s Hospital maps and data used in this
+          application are copyrighted and provided for the sole use of
+          educational purposes.
+        </h6>
+        <div className={"logo-container"}>
+          <img src={WPI_Logo} alt="WPI Logo" style={{ maxWidth: "150px" }} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreditsPage;
