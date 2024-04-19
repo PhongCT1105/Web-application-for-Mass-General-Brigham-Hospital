b220591093e5e3b7d8082583dab50acb51651d72
echoct21 <41646433+echoct21@users.noreply.github.com>
2024-04-19 16:34:20 -0400

    Updated about us to use Shadcn

--- apps/frontend/src/components/blocks/AboutBlock.tsx
+++ apps/frontend/src/components/blocks/AboutBlock.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { Card } from "@/components/ui/card.tsx";
+import { Button } from "@/components/ui/button.tsx";
 
 interface AboutInfo {
   Name: string;
   role?: string;
   devrole?: string;
-  ClassYear: string;
-  Major: string;
+  ClassYear?: string;
+  major?: string;
   Email: string;
   Github?: string;
   Linkdin?: string;
   Imagepath: string;
-  FunFact: string;
+  FunFact?: string;
 }
 
 const AboutBlock = ({
@@ -20,7 +22,7 @@ const AboutBlock = ({
   role,
   devrole,
   ClassYear,
-  Major,
+  major,
   Email,
   Github,
   Linkdin,
@@ -32,58 +34,43 @@ const AboutBlock = ({
   const handleClose = () => setOpen(false);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        maxWidth: 700,
-        padding: "10px",
-        cursor: "pointer",
-        backgroundColor: "#e9e8e9",
-      }}
+    <Card
+      className={"flex flex-col justify-space-between mw-700 p-5 bg-gray-200"}
     >
       <img
         src={Imagepath}
         alt={Name}
         style={{ height: 250, width: 250, objectFit: "cover" }}
+        className={"object-fit-cover rounded-md"}
         onClick={handleOpen}
       />
 
       {open && (
-        <div
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 600,
-            backgroundColor: "white",
-            border: "0px solid #000 !important",
-            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
-            padding: "20px",
-          }}
-        >
+        <Card className={"position-relative mw-250 bg-white shadow-sm p-2"}>
           <h2>Favorite Quote:</h2>
           <p>{FunFact}</p>
-          <button onClick={handleClose}>Close</button>
-        </div>
+          <Button onClick={handleClose}>Close</Button>
+        </Card>
       )}
 
       <div>
-        <h5>{Name}</h5>
+        <h4 className={"text-center font-bold"}>{Name}</h4>
         {role && <p>Role: {role}</p>}
         {devrole && <p>Software Role: {devrole}</p>}
         <p>Class Year: {ClassYear}</p>
-        <p>Major: {Major}</p>
+        {major && <p>Major: {major}</p>}
         <p>Email: {Email}</p>
       </div>
 
-      <div style={{ display: "flex", alignItems: "end" }}>
-        {Github && <GitHubLogoIcon />}
-        {Linkdin && <LinkedInLogoIcon />}
+      <div className={"flex justify-content-between"}>
+        <Button variant={"invisible"} className={"ml-auto"}>
+          {Github && <GitHubLogoIcon />}
+        </Button>
+        <Button variant={"invisible"} className={"mr-auto"}>
+          {Linkdin && <LinkedInLogoIcon />}
+        </Button>
       </div>
-    </div>
+    </Card>
   );
 };
 
--- apps/frontend/src/routes/AboutUsPage.tsx
+++ apps/frontend/src/routes/AboutUsPage.tsx
@@ -1,7 +1,6 @@
 import "../styles/example.route.css";
 import "../styles/globals.css";
 import AboutBlock from "@/components/blocks/AboutBlock.tsx";
-import AboutBlockFaculty from "@/components/blocks/AboutFacultyBlock.tsx";
 import Ari from "@/assets/headshots/Ari.png";
 import Katy from "@/assets/headshots/Katy.jpg";
 import Phong from "@/assets/headshots/Phong.png";
@@ -17,6 +16,7 @@ import Shettler from "@/assets/headshots/Shettler.png";
 import Owen from "@/assets/headshots/Owen.jpg";
 import WPI_Logo from "@/assets/WPI_Logo.png";
 import Wong from "@/assets/headshots/Wong.png";
+import { Card } from "@/components/ui/card.tsx";
 
 // import wongAsters from "@/assets/wongSunFlower.png";
 // import wongSunflower from "@/assets/wongAsters.png";
@@ -41,7 +41,7 @@ const AboutMePage = () => {
           Schechter and Katy Stuparu.
         </h4>
         <h1 style={{ textAlign: "center", paddingTop: "70px" }}>Faculty</h1>
-        <div className="faculty-flex">
+        <Card className="faculty-flex p-5">
           <div
             style={{
               display: "flex",
@@ -52,7 +52,7 @@ const AboutMePage = () => {
               margin: "auto",
             }}
           >
-            <AboutBlockFaculty
+            <AboutBlock
               Name={"Wilson Wong"}
               role={"Professor"}
               Email={"wwong2@wpi.edu"}
@@ -60,227 +60,190 @@ const AboutMePage = () => {
               Linkdin={"https://www.linkedin.com/in/wilson-wong-wpi/"}
               Imagepath={Wong}
             />
-            <AboutBlockFaculty
+            <AboutBlock
               Name={"Ari Schechter"}
               role={"Team Coach"}
               ClassYear={"2025"}
-              Major={"CS"}
+              major={"CS"}
               Email={"aschechter@wpi.edu"}
               Github={"https://github.com/aschechter10"}
               Linkdin={"https://www.linkedin.com/in/ari-schechter/"}
               Imagepath={Ari}
             />
-            <AboutBlockFaculty
+            <AboutBlock
               Name={"Katy Stuparu"}
               role={"Team Coach"}
               ClassYear={"2025"}
-              Major={"CS"}
+              major={"CS"}
               Email={"kstuparu@wpi.edu"}
               Github={"https://github.com/kastuparu"}
               Linkdin={"https://www.linkedin.com/in/kastuparu/"}
               Imagepath={Katy}
             />
           </div>
-          <h1 style={{ textAlign: "center", paddingTop: "100px" }}>Our Team</h1>
-          <div className="team-members-flex">
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                paddingTop: "20px",
-                justifyContent: "space-between",
-              }}
-            >
-              <AboutBlock
-                Name={"Phong Cao"}
-                role={"Team Lead"}
-                devrole={"Full-Stack Engineer"}
-                ClassYear={"2027"}
-                Major={"CS & DS"}
-                Email={"pcao@wpi.edu"}
-                Github={"https://github.com/PhongCT1105"}
-                Linkdin={"https://www.linkedin.com/in/phong-cao/"}
-                Imagepath={Phong}
-                FunFact={""}
-              />
+        </Card>
+        <h1 style={{ textAlign: "center", paddingTop: "100px" }}>Our Team</h1>
+        <Card className="team-members-flex p-5">
+          <div className={"flex flex-row pt-10 pb-5 justify-between"}>
+            <AboutBlock
+              Name={"Phong Cao"}
+              role={"Team Lead"}
+              devrole={"Full-Stack Engineer"}
+              ClassYear={"2027"}
+              major={"CS & DS"}
+              Email={"pcao@wpi.edu"}
+              Github={"https://github.com/PhongCT1105"}
+              Linkdin={"https://www.linkedin.com/in/phong-cao/"}
+              Imagepath={Phong}
+              FunFact={""}
+            />
 
-              <AboutBlock
-                Name={"Alexander Lap"}
-                role={"Frontend Lead"}
-                devrole={"Full-Stack Engineer"}
-                ClassYear={"2026"}
-                Major={"CS"}
-                Email={"ajlap@wpi.edu"}
-                Github={"https://github.com/alexanderjlap"}
-                Linkdin={"https://www.linkedin.com/in/alexanderlap/"}
-                Imagepath={Lap}
-                FunFact={""}
-              />
-              <AboutBlock
-                Name={"Trang Tran"}
-                role={"Backend Lead"}
-                devrole={"Full-Stack Engineer"}
-                ClassYear={"2026"}
-                Major={"CS & IMGD"}
-                Email={"tntran@wpi.edu"}
-                Github={"https://github.com/tsnhim"}
-                Linkdin={"https://www.linkedin.com/in/tntran03/"}
-                Imagepath={Trang}
-                FunFact={""}
-              />
-            </div>
-            <div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  paddingTop: "20px",
-                  paddingBottom: "10px",
-                  justifyContent: "space-between",
-                }}
-              >
+            <AboutBlock
+              Name={"Alexander Lap"}
+              role={"Frontend Lead"}
+              devrole={"Full-Stack Engineer"}
+              ClassYear={"2026"}
+              major={"CS"}
+              Email={"ajlap@wpi.edu"}
+              Github={"https://github.com/alexanderjlap"}
+              Linkdin={"https://www.linkedin.com/in/alexanderlap/"}
+              Imagepath={Lap}
+              FunFact={""}
+            />
+            <AboutBlock
+              Name={"Trang Tran"}
+              role={"Backend Lead"}
+              devrole={"Full-Stack Engineer"}
+              ClassYear={"2026"}
+              major={"CS & IMGD"}
+              Email={"tntran@wpi.edu"}
+              Github={"https://github.com/tsnhim"}
+              Linkdin={"https://www.linkedin.com/in/tntran03/"}
+              Imagepath={Trang}
+              FunFact={""}
+            />
+          </div>
+          <div className={"flex flex-row pt-10 pb-5 justify-between"}>
+            <AboutBlock
+              Name={"Mina Boktor"}
+              role={"Frontend Engineer"}
+              devrole={"Full-Stack Engineer"}
+              ClassYear={"2026"}
+              major={"CS"}
+              Email={"mboktor@wpi.edu"}
+              Github={"https://github.com/minaboktor2628"}
+              Linkdin={"https://www.linkedin.com/in/mina-boktor-108126211/"}
+              Imagepath={Mina}
+              FunFact={""}
+            />
+            <AboutBlock
+              Name={"Henry Solomon"}
+              role={"Frontend Engineer"}
+              devrole={"Full-Stack Engineer"}
+              ClassYear={"2026"}
+              major={"CS"}
+              Email={"hjsolomon@wpi.edu"}
+              Github={"https://github.com/hjsolomon"}
+              Linkdin={"https://www.linkedin.com/in/hjsolomon/"}
+              Imagepath={Henry}
+              FunFact={""}
+            />
+            <AboutBlock
+              Name={"June Whittall"}
+              role={"Backend Engineer"}
+              devrole={"Full-Stack Engineer"}
+              ClassYear={"2026"}
+              major={"CS & IMGD"}
+              Email={"jwhittall@wpi.edu"}
+              Github={"https://github.com/jwwhittall"}
+              Linkdin={""}
+              Imagepath={June}
+              FunFact={""}
+            />
+          </div>
+          <div className={"flex flex-row pt-10 pb-5 justify-between"}>
+            <AboutBlock
+              Name={"Tracy Yang"}
+              role={"Algorithms Enginner"}
+              devrole={"Full-Stack Engineer"}
+              ClassYear={"2025"}
+              major={"RBE"}
+              Email={"tdyang@wpi.edu"}
+              Github={"https://github.com/tdyang"}
+              Linkdin={"https://www.linkedin.com/in/tracy-yang-b61b72211/"}
+              Imagepath={Tracy}
+              FunFact={""}
+            />
+            <AboutBlock
+              Name={"Alexsandra Antoski"}
+              role={"Product Manager"}
+              devrole={"Full-Stack Engineer"}
+              ClassYear={"2025"}
+              major={"CS"}
+              Email={"aantoski@wpi.edu"}
+              Github={"https://github.com/aantoski"}
+              Linkdin={
+                "https://www.linkedin.com/in/alexsandra-antoski-9b7a242a4/"
+              }
+              Imagepath={Sandi}
+              FunFact={""}
+            />
+            <AboutBlock
+              Name={"Alexander Kraemling"}
+              role={"Product Owner"}
+              devrole={"Full-Stack Engineer"}
+              ClassYear={"2026"}
+              major={"CS & RBE"}
+              Email={"ajkraemling@wpi.edu"}
+              Github={"https://github.com/ajkraemling"}
+              Linkdin={
+                "https://www.linkedin.com/in/alexander-kraemling-aa9b42214/"
+              }
+              Imagepath={Kraemling}
+              FunFact={""}
+            />
+          </div>
+          <div className={"mx-auto"}>
+            <div className={"w-3/4"}>
+              <div className={"flex flex-row pt-10 pb-5 justify-between"}>
                 <AboutBlock
-                  Name={"Mina Boktor"}
-                  role={"Frontend Engineer"}
+                  Name={"Alexander Shettler"}
+                  role={"Scrum Master"}
                   devrole={"Full-Stack Engineer"}
                   ClassYear={"2026"}
-                  Major={"CS"}
-                  Email={"mboktor@wpi.edu"}
-                  Github={"https://github.com/minaboktor2628"}
-                  Linkdin={"https://www.linkedin.com/in/mina-boktor-108126211/"}
-                  Imagepath={Mina}
-                  FunFact={""}
-                />
-                <AboutBlock
-                  Name={"Henry Solomon"}
-                  role={"Frontend Engineer"}
-                  devrole={"Full-Stack Engineer"}
-                  ClassYear={"2026"}
-                  Major={"CS"}
-                  Email={"hjsolomon@wpi.edu"}
-                  Github={"https://github.com/hjsolomon"}
-                  Linkdin={"https://www.linkedin.com/in/hjsolomon/"}
-                  Imagepath={Henry}
-                  FunFact={""}
-                />
-                <AboutBlock
-                  Name={"June Whittall"}
-                  role={"Backend Engineer"}
-                  devrole={"Full-Stack Engineer"}
-                  ClassYear={"2026"}
-                  Major={"CS & IMGD"}
-                  Email={"jwhittall@wpi.edu"}
-                  Github={"https://github.com/jwwhittall"}
+                  major={"CS"}
+                  Email={"aeshettler@wpi.edu"}
+                  Github={"https://github.com/alexshettler"}
                   Linkdin={""}
-                  Imagepath={June}
+                  Imagepath={Shettler}
+                  FunFact={""}
+                />
+                <AboutBlock
+                  Name={"Owen Lacey"}
+                  role={"Documentation Analyst"}
+                  devrole={"Full-Stack Engineer"}
+                  ClassYear={"2026"}
+                  major={"IMGD"}
+                  Email={"oelacey@wpi.edu"}
+                  Github={"https://github.com/echoct21"}
+                  Linkdin={"https://www.linkedin.com/in/owen-lacey-721205250/"}
+                  Imagepath={Owen}
                   FunFact={""}
                 />
               </div>
-              <div />
-              <div>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    paddingTop: "20px",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <AboutBlock
-                    Name={"Tracy Yang"}
-                    role={"Algorithms Enginner"}
-                    devrole={"Full-Stack Engineer"}
-                    ClassYear={"2025"}
-                    Major={"RBE"}
-                    Email={"tdyang@wpi.edu"}
-                    Github={"https://github.com/tdyang"}
-                    Linkdin={
-                      "https://www.linkedin.com/in/tracy-yang-b61b72211/"
-                    }
-                    Imagepath={Tracy}
-                    FunFact={""}
-                  />
-                  <AboutBlock
-                    Name={"Alexsandra Antoski"}
-                    role={"Product Manager"}
-                    devrole={"Full-Stack Engineer"}
-                    ClassYear={"2025"}
-                    Major={"CS"}
-                    Email={"aantoski@wpi.edu"}
-                    Github={"https://github.com/aantoski"}
-                    Linkdin={
-                      "https://www.linkedin.com/in/alexsandra-antoski-9b7a242a4/"
-                    }
-                    Imagepath={Sandi}
-                    FunFact={""}
-                  />
-                  <AboutBlock
-                    Name={"Alexander Kraemling"}
-                    role={"Product Owner"}
-                    devrole={"Full-Stack Engineer"}
-                    ClassYear={"2026"}
-                    Major={"CS & RBE"}
-                    Email={"ajkraemling@wpi.edu"}
-                    Github={"https://github.com/ajkraemling"}
-                    Linkdin={
-                      "https://www.linkedin.com/in/alexander-kraemling-aa9b42214/"
-                    }
-                    Imagepath={Kraemling}
-                    FunFact={""}
-                  />
-                </div>
-                <div />
-                <div>
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      paddingTop: "20px",
-                      justifyContent: "space-evenly",
-                    }}
-                  >
-                    <AboutBlock
-                      Name={"Alexander Shettler"}
-                      role={"Scrum Master"}
-                      devrole={"Full-Stack Engineer"}
-                      ClassYear={"2026"}
-                      Major={"CS"}
-                      Email={"aeshettler@wpi.edu"}
-                      Github={"https://github.com/alexshettler"}
-                      Linkdin={""}
-                      Imagepath={Shettler}
-                      FunFact={""}
-                    />
-                    <AboutBlock
-                      Name={"Owen Lacey"}
-                      role={"Documentation Analyst"}
-                      devrole={"Full-Stack Engineer"}
-                      ClassYear={"2026"}
-                      Major={"IMGD"}
-                      Email={"oelacey@wpi.edu"}
-                      Github={"https://github.com/echoct21"}
-                      Linkdin={
-                        "https://www.linkedin.com/in/owen-lacey-721205250/"
-                      }
-                      Imagepath={Owen}
-                      FunFact={""}
-                    />
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
-          <h6 style={{ textAlign: "center", marginTop: "75px" }}>
-            WPI Computer Science Department, CS3733-D24 Software Engineering
-          </h6>
-          <h6 style={{ textAlign: "center", marginTop: "20px" }}>
-            ©The Brigham & Women’s Hospital maps and data used in this
-            application are copyrighted and provided for the sole use of
-            educational purposes.
-          </h6>
-          <div className={"logo-container"}>
-            <img src={WPI_Logo} alt="WPI Logo" style={{ maxWidth: "150px" }} />
-          </div>
+        </Card>
+        <h6 style={{ textAlign: "center", marginTop: "75px" }}>
+          WPI Computer Science Department, CS3733-D24 Software Engineering
+        </h6>
+        <h6 style={{ textAlign: "center", marginTop: "20px" }}>
+          ©The Brigham & Women’s Hospital maps and data used in this
+          application are copyrighted and provided for the sole use of
+          educational purposes.
+        </h6>
+        <div className={"logo-container"}>
+          <img src={WPI_Logo} alt="WPI Logo" style={{ maxWidth: "150px" }} />
         </div>
       </div>
     </div>
