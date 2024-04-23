cc5650bcf642ce7d56d977329ddfe390bea561e3
Phong Cao <104048448+PhongCT1105@users.noreply.github.com>
2024-04-23 18:48:33 -0400

    Update data props

--- apps/frontend/src/data/chartValue/lineChartData.ts
+++ apps/frontend/src/data/chartValue/lineChartData.ts
@@ -1,31 +1,28 @@
+const data = [
+  { months: "January", user: 150, staff: 200 },
+  { months: "February", user: 150, staff: 200 },
+  { months: "March", user: 150, staff: 200 },
+  { months: "April", user: 150, staff: 200 },
+  { months: "May", user: 150, staff: 200 },
+  { months: "June", user: 150, staff: 200 },
+  { months: "July", user: 150, staff: 200 },
+  { months: "August", user: 150, staff: 200 },
+  { months: "September", user: 150, staff: 200 },
+  { months: "October", user: 150, staff: 200 },
+  { months: "November", user: 150, staff: 200 },
+  { months: "December", user: 150, staff: 200 },
+];
 export const lineChartData = {
-  labels: [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ],
+  labels: data.map((map) => map.months),
   datasets: [
     {
       label: "Users",
-      data: [
-        3000, 5000, 4500, 6000, 8000, 7000, 9000, 6000, 8000, 9000, 2000, 4000,
-      ],
+      data: data.map((map) => map.user),
       borderColor: "rgb(75, 192, 192)",
     },
     {
       label: "Staff",
-      data: [
-        4000, 4500, 3000, 2500, 7000, 6000, 5000, 4000, 7000, 2500, 3000, 6000,
-      ],
+      data: data.map((map) => map.staff),
       borderColor: "red",
     },
   ],
