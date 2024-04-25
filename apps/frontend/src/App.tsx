690dc18e669221d32d1eaceb1d8f6901296d14c4
Phong Cao <104048448+PhongCT1105@users.noreply.github.com>
2024-04-25 03:23:10 -0400

    Add Dashboard component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -20,7 +20,7 @@ import { Header } from "@/components/blocks/header.tsx";
 import { MapEditorTablePage } from "@/routes/map-editor/mapEditorTablePage.tsx";
 import InsightRoute from "@/routes/InsightRoute.tsx";
 import InstructionsPage from "./routes/InstructionsPage.tsx";
-import DashBoard from "./routes/DashBoard.tsx";
+import DashBoard from "./routes/service-request/DashBoard/DashBoard.tsx";
 // import { HeaderHome } from "@/components/blocks/headerHome.tsx";
 
 function App() {
@@ -48,11 +48,6 @@ function App() {
           path: "/request-log-Page",
           errorElement: <h1>ERROR</h1>,
           element: <RequestLogPage />,
-        },
-        {
-          path: "/dashboard",
-          errorElement: <h1>ERROR</h1>,
-          element: <AboutCreditsPages />,
         },
         {
           path: "/about-us",
---
+++ apps/frontend/src/routes/service-request/DashBoard/DashBoard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import BarGraph from "@/components/Graph/BarGraph.tsx";
+import { overallChartData } from "@/data/overallData/barChartData.ts";
+
+const Dashboard = () => {
+  return (
+    <div className="flex flex-col h-full">
+      {/* Header */}
+      <header className="bg-blue-300 text-white flex justify-center">
+        <h1 className="text-2xl font-bold">Statistic Dashboard</h1>
+      </header>
+
+      {/* Main Content */}
+      <main className="flex-grow bg-gray-100 p-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          {/* Card 1 */}
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold mb-2">Total Users</h2>
+            <p className="text-3xl font-bold">1,000</p>
+          </div>
+
+          {/* Card 2 */}
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold mb-2">Total Sales</h2>
+            <p className="text-3xl font-bold">$10,000</p>
+          </div>
+
+          {/* Card 3 */}
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold mb-2">Total Orders</h2>
+            <p className="text-3xl font-bold">500</p>
+          </div>
+
+          {/* Card 4 */}
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold mb-2">Total Revenue</h2>
+            <p className="text-3xl font-bold">$20,000</p>
+          </div>
+        </div>
+        <BarGraph props={overallChartData} />
+      </main>
+    </div>
+  );
+};
+
+export default Dashboard;
