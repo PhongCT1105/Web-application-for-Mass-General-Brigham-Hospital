fc610a683e7bb3a30903da797612b8a81a7f226e
aantoski <aantoski96@gmail.com>
2024-04-26 18:57:05 -0400

    fixed a lil buggy

--- apps/frontend/src/components/blocks/MapBlock.tsx
+++ apps/frontend/src/components/blocks/MapBlock.tsx
@@ -236,6 +236,18 @@ export const MapBlock: React.FC = () => {
     [],
   );
 
+  const PathMarkers: { [key: string]: L.LayerGroup } = useMemo(
+    () =>
+      ({
+        L1: new L.LayerGroup(),
+        L2: new L.LayerGroup(),
+        1: new L.LayerGroup(),
+        2: new L.LayerGroup(),
+        3: new L.LayerGroup(),
+      }) as const,
+    [],
+  );
+
   const loadData = async () => {
     const { data: nodeData } = await axios.get(`/api/mapreq/nodes`);
 
@@ -517,6 +529,7 @@ export const MapBlock: React.FC = () => {
         SpecialMarkers[key].addTo(Layers[key]);
         StartMarker[key].addTo(Layers[key]);
         EndMarker[key].addTo(Layers[key]);
+        PathMarkers[key].addTo(Layers[key]);
         L.imageOverlay(FloorImages[key], bounds).addTo(Layers[key]);
       });
     }
@@ -535,6 +548,7 @@ export const MapBlock: React.FC = () => {
     StartMarker,
     EndMarker,
     addMarker,
+    PathMarkers,
   ]); // Dependency array
 
   function drawPath(start: string, end: string) {
@@ -559,7 +573,7 @@ export const MapBlock: React.FC = () => {
     return L.polyline([startCoords, endCoords], {
       color: "blue",
       weight: 5,
-      dashArray: "3, 10",
+      // dashArray: "3, 10",
       snakingSpeed: 200,
       snakeRepeat: true,
     });
@@ -768,7 +782,6 @@ export const MapBlock: React.FC = () => {
       StartMarker[key].clearLayers();
       EndMarker[key].clearLayers();
     });
-
     placeStartEndMarkers(searchPath);
     placeFloorMarkers(searchPath);
 
@@ -801,6 +814,7 @@ export const MapBlock: React.FC = () => {
       StartMarker[key].clearLayers();
       EndMarker[key].clearLayers();
       Paths[key].clearLayers();
+      PathMarkers[key].clearLayers();
       Markers[key].addTo(Layers[key]);
     });
     setTextDirections([]);
@@ -840,22 +854,26 @@ export const MapBlock: React.FC = () => {
     path().then(() => {
       handleClear();
       clearMarkers();
-      addMarkersOnPath(nodeArray);
       addPathPolylines(nodeArray);
+      addMarkersOnPath(nodeArray);
       createTextDirections(nodeArray); //nodeArray[0].floor);
     });
   }
 
   function addMarkersOnPath(searchPath: Node[]) {
+    let index = 0;
     searchPath.forEach((node) => {
-      const coords: [number, number] = [3400 - node.ycoord, node.xcoord];
-      const marker = L.circleMarker(coords, {
-        radius: 5,
-        color: "#ebd234",
-        fillColor: "#ebd234",
-        fillOpacity: 1,
-      }).bindPopup(node.longName);
-      marker.addTo(Paths[node.floor]);
+      if (index != 0 || index != searchPath.length - 1) {
+        const coords: [number, number] = [3400 - node.ycoord, node.xcoord];
+        const marker = L.circleMarker(coords, {
+          radius: 8,
+          color: "#ebd234",
+          fillColor: "blue",
+          fillOpacity: 1,
+        }).bindPopup(node.longName);
+        marker.addTo(PathMarkers[node.floor]);
+        index++;
+      }
     });
   }
 
