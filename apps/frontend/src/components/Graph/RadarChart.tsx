5c9c1bc67ad0118c290407b656466241a15182f4
Phong Cao <104048448+PhongCT1105@users.noreply.github.com>
2024-04-30 15:18:10 -0400

    Update overall

--- apps/frontend/src/components/Graph/RadarChart.tsx
+++ apps/frontend/src/components/Graph/RadarChart.tsx
@@ -8,35 +8,48 @@ import {
   ChartType,
 } from "chart.js";
 import { Radar } from "react-chartjs-2";
+import { radarRequestData } from "@/components/Graph/GraphInterface/radarRequestData.tsx";
 
 ChartJS.register(LineElement, PointElement, Tooltip, Legend, RadialLinearScale);
 
-function RadarChart() {
+interface Props {
+  props: radarRequestData[];
+}
+
+function RadarChart({ props }: Props) {
+  // Define a mapping object for label abbreviations
+  const labelMappings: Record<string, string> = {
+    F: "Flower Request",
+    ME: "Medication Request",
+    T: "Patient Transport Request",
+    SA: "Sanitation Request",
+    SE: "Security Request",
+    MA: "Maintenance Request",
+  };
+
+  // Map the labels from data.service to the corresponding abbreviations
+  const labels: string[] = props.map((data) => labelMappings[data.service]);
+
   const data = {
-    labels: [
-      "Flower Request",
-      "Medication Request",
-      "Patient Transport Request",
-      "Sanitation Request",
-      "Security Request",
-      "Maintenance Request",
-    ],
+    labels: labels, // Provide labels directly here
     datasets: [
       {
-        labels: "Request",
-        data: [20, 10, 30, 40, 50, 60],
+        label: "Total Service Request Used", // Use label instead of labels
+        data: props.map((data) => data.total),
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
       },
     ],
   };
   const options = {
-    type: "bar" as ChartType,
+    type: "radar" as ChartType, // Set chart type to "radar"
     responsive: true,
     maintainAspectRatio: true,
     aspectRatio: 2.5,
     plugins: {
       title: {
         display: true,
-        text: "Bar Chart of total service request used",
+        text: "Radar Chart of total service request used", // Update chart title
       },
     },
   };
