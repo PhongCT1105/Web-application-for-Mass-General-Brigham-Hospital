d0c058c3fc8f22e887101b4b96f84dfa7497fefe
aantoski <aantoski96@gmail.com>
2024-04-30 21:27:50 -0400

    fixed Dashboard link

--- apps/frontend/src/components/blocks/header.tsx
+++ apps/frontend/src/components/blocks/header.tsx
@@ -286,7 +286,7 @@ export function Header() {
                 </>
               )}
               <a
-                href="/apps/frontend/src/routes/Dashboard"
+                href="/Dashboard"
                 className={`transition-colors hover:text-yellow-500 text-gray-300 ${
                   location.pathname === "/dashboard"
                     ? "text-yellow-500"
